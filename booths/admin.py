c20d6f896dcb62c2c1d1602b7aef5441115f7c2b
yewon <shimyewon@ewhain.net>
2024-04-23 14:52:03 +0900

    fix: admin 수정

--- booths/admin.py
+++ booths/admin.py
@@ -24,7 +24,3 @@ class CommentAdmin(admin.ModelAdmin):
 class EvnetAdmin(admin.ModelAdmin):
 	list_display = ['id', 'name', 'user', 'created_at', 'updated_at']
 	list_display_links = ['id','name']
-
-@admin.register(EventComment)
-class EventCommentAdmin(admin.ModelAdmin):
-	list_display = ['id', 'user', 'event', 'content', 'created_at', 'updated_at']
