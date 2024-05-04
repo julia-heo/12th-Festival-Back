d5670d622e2e3061d5c5e1aa465cc4ef5dbe7dc8
chaerin <100216331+julia-heo@users.noreply.github.com>
2024-05-04 15:04:33 +0900

    fix: 부스리스트 college추가

--- booths/views.py
+++ booths/views.py
@@ -170,7 +170,7 @@ class BoothListView(views.APIView):
         if day:
             booths = booths.filter(days__date=day)
         if college:
-            if college is not None:
+            if college is not "null":
                 booths = booths.filter(college=college)
         total = len(booths)
         total_page = math.ceil(total/10)
